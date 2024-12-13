149b9b28140c241547274be567a0dc58e37d8c75
Camilleri <ssancam391@g.educaand.es>
2024-12-13 16:58:38 +0100

    AnimalDTO

---
+++ src/main/java/com/refugioKimba/dto/AnimalDTO.java
@@ -0,0 +1,55 @@
+package com.refugioKimba.dto;
+
+import com.refugioKimba.model.EstadoAnimal;
+import com.refugioKimba.model.TipoAnimal;
+
+import java.time.LocalDate;
+
+public class AnimalDTO {
+    private String nombre;
+    private TipoAnimal tipo;
+    private EstadoAnimal estado;
+    private LocalDate registro;
+
+    public AnimalDTO() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public AnimalDTO(String nombre, TipoAnimal tipo, EstadoAnimal estado, LocalDate registro) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.registro = registro;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public TipoAnimal getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoAnimal tipo) {
+        this.tipo = tipo;
+    }
+
+    public EstadoAnimal getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoAnimal estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(LocalDate registro) {
+        this.registro = registro;
+    }
+}
---
+++ src/main/java/com/refugioKimba/dto/UsuarioDTO.java
@@ -0,0 +1,4 @@
+package com.refugioKimba.dto;
+
+public class UsuarioDTO {
+}
